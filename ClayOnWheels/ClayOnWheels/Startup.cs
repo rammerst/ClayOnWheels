64e650ab4756bfea038e95814f79b8516109d740
brambarnard <brambarnard@gmail.com>
2016-11-07 19:58:09 +0100

    Project created - intial commit

---
+++ ClayOnWheels/ClayOnWheels/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClayOnWheels.Startup))]
+namespace ClayOnWheels
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
