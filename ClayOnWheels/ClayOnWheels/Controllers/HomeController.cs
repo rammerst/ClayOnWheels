32a5eb671a363efc201f771d3993a97566abfaf7
brambarnard <brambarnard@gmail.com>
2016-11-13 22:41:44 +0100

    Add calendar

--- ClayOnWheels/ClayOnWheels/Controllers/HomeController.cs
+++ ClayOnWheels/ClayOnWheels/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClayOnWheels.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,8 +13,60 @@ namespace ClayOnWheels.Controllers
         {
             return View();
         }
+        public string Init()
+        {
+            bool rslt = Utils.InitialiseDiary();
+            return rslt.ToString();
+        }
 
-        public ActionResult About()
+        public void UpdateEvent(int id, string NewEventStart, string NewEventEnd)
+        {
+            DiaryEvent.UpdateDiaryEvent(id, NewEventStart, NewEventEnd);
+        }
+
+
+        public bool SaveEvent(string Title, string NewEventDate, string NewEventTime, string NewEventDuration)
+        {
+            return DiaryEvent.CreateNewEvent(Title, NewEventDate, NewEventTime, NewEventDuration);
+        }
+
+        public JsonResult GetDiarySummary(double start, double end)
+        {
+            var ApptListForDate = DiaryEvent.LoadAppointmentSummaryInDateRange(start, end);
+            var eventList = from e in ApptListForDate
+                            select new
+                            {
+                                id = e.ID,
+                                title = e.Title,
+                                start = e.StartDateString,
+                                end = e.EndDateString,
+                                someKey = e.SomeImportantKeyID,
+                                allDay = false
+                            };
+            var rows = eventList.ToArray();
+            return Json(rows, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetDiaryEvents(double start, double end)
+        {
+            var ApptListForDate = DiaryEvent.LoadAllAppointmentsInDateRange(start, end);
+            var eventList = from e in ApptListForDate
+                            select new
+                            {
+                                id = e.ID,
+                                title = e.Title,
+                                start = e.StartDateString,
+                                end = e.EndDateString,
+                                color = e.StatusColor,
+                                className = e.ClassName,
+                                someKey = e.SomeImportantKeyID,
+                                allDay = false
+                            };
+            var rows = eventList.ToArray();
+            return Json(rows, JsonRequestBehavior.AllowGet);
+        }
+    
+    public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
