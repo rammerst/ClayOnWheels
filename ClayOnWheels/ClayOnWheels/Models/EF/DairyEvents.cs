5b3b8d8fd33106556c66cd6e037237de0f256e43
bram barnard <brambarnard@gmail.com>
2016-11-15 09:27:04 +0100

    Add EF

--- ClayOnWheels/ClayOnWheels/Models/EF/DairyEvents.cs
+++ ClayOnWheels/ClayOnWheels/Models/EF/DairyEvents.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Globalization; // << dont forget to add this for converting dates to localtime
 using System.Data.Entity.Core.Objects;
 
-namespace ClayOnWheels.Models
+namespace ClayOnWheels.Models.EF
 {
     public class DiaryEvent
     {
@@ -24,30 +24,30 @@ namespace ClayOnWheels.Models
         {
             var fromDate = ConvertFromUnixTimestamp(start);
             var toDate = ConvertFromUnixTimestamp(end);
-            using (DiaryContainer ent = new DiaryContainer())
+            using (var ent = new MyDbContext())
             {
-                var rslt = ent.AppointmentDiary.Where(s => s.DateTimeScheduled >= fromDate && EntityFunctions.AddMinutes(s.DateTimeScheduled, s.AppointmentLength) <= toDate);
+                var rslt = ent.AppointmentDiaries.Where(s => s.DateTimeScheduled >= fromDate && EntityFunctions.AddMinutes(s.DateTimeScheduled, s.AppointmentLength) <= toDate);
 
                 List<DiaryEvent> result = new List<DiaryEvent>();
                 foreach (var item in rslt)
                 {
                     DiaryEvent rec = new DiaryEvent();
-                    rec.ID = item.ID;
+                    rec.ID = item.Id;
                     rec.SomeImportantKeyID = item.SomeImportantKey;
                     rec.StartDateString = item.DateTimeScheduled.ToString("s"); // "s" is a preset format that outputs as: "2009-02-27T12:12:22"
                     rec.EndDateString = item.DateTimeScheduled.AddMinutes(item.AppointmentLength).ToString("s"); // field AppointmentLength is in minutes
                     rec.Title = item.Title + " - " + item.AppointmentLength.ToString() + " mins";
-                    rec.StatusString = Enums.GetName<AppointmentStatus>((AppointmentStatus)item.StatusENUM);
+                    rec.StatusString = Enums.GetName<AppointmentStatus>((AppointmentStatus)item.StatusEnum);
                     rec.StatusColor = Enums.GetEnumDescription<AppointmentStatus>(rec.StatusString);
                     string ColorCode = rec.StatusColor.Substring(0, rec.StatusColor.IndexOf(":"));
-                    rec.ClassName = rec.StatusColor.Substring(rec.StatusColor.IndexOf(":")+1, rec.StatusColor.Length - ColorCode.Length-1);
-                    rec.StatusColor = ColorCode;                   
+                    rec.ClassName = rec.StatusColor.Substring(rec.StatusColor.IndexOf(":") + 1, rec.StatusColor.Length - ColorCode.Length - 1);
+                    rec.StatusColor = ColorCode;
                     result.Add(rec);
                 }
 
                 return result;
             }
-        
+
         }
 
 
@@ -56,9 +56,9 @@ namespace ClayOnWheels.Models
 
             var fromDate = ConvertFromUnixTimestamp(start);
             var toDate = ConvertFromUnixTimestamp(end);
-            using (DiaryContainer ent = new DiaryContainer())
+            using (var ent = new FakeMyDbContext())
             {
-                var rslt = ent.AppointmentDiary.Where(s => s.DateTimeScheduled >= fromDate && EntityFunctions.AddMinutes(s.DateTimeScheduled, s.AppointmentLength) <= toDate)
+                var rslt = ent.AppointmentDiaries.Where(s => s.DateTimeScheduled >= fromDate && EntityFunctions.AddMinutes(s.DateTimeScheduled, s.AppointmentLength) <= toDate)
                                                         .GroupBy(s => EntityFunctions.TruncateTime(s.DateTimeScheduled))
                                                         .Select(x => new { DateTimeScheduled = x.Key, Count = x.Count() });
 
@@ -68,10 +68,10 @@ namespace ClayOnWheels.Models
                 {
                     DiaryEvent rec = new DiaryEvent();
                     rec.ID = i; //we dont link this back to anything as its a group summary but the fullcalendar needs unique IDs for each event item (unless its a repeating event)
-                    rec.SomeImportantKeyID = -1;  
+                    rec.SomeImportantKeyID = -1;
                     string StringDate = string.Format("{0:yyyy-MM-dd}", item.DateTimeScheduled);
                     rec.StartDateString = StringDate + "T00:00:00"; //ISO 8601 format
-                    rec.EndDateString = StringDate +"T23:59:59";
+                    rec.EndDateString = StringDate + "T23:59:59";
                     rec.Title = "Booked: " + item.Count.ToString();
                     result.Add(rec);
                     i++;
@@ -79,22 +79,24 @@ namespace ClayOnWheels.Models
 
                 return result;
             }
-        
+
         }
 
-        public static void UpdateDiaryEvent(int id, string NewEventStart, string NewEventEnd) 
+        public static void UpdateDiaryEvent(int id, string NewEventStart, string NewEventEnd)
         {
             // EventStart comes ISO 8601 format, eg:  "2000-01-10T10:00:00Z" - need to convert to DateTime
-            using (DiaryContainer ent = new DiaryContainer()) {
-                var rec = ent.AppointmentDiary.FirstOrDefault(s => s.ID == id);
+            using (var ent = new MyDbContext())
+            {
+                var rec = ent.AppointmentDiaries.FirstOrDefault(s => s.Id == id);
                 if (rec != null)
                 {
                     DateTime DateTimeStart = DateTime.Parse(NewEventStart, null, DateTimeStyles.RoundtripKind).ToLocalTime(); // and convert offset to localtime
                     rec.DateTimeScheduled = DateTimeStart;
-                    if (!String.IsNullOrEmpty(NewEventEnd)) { 
+                    if (!String.IsNullOrEmpty(NewEventEnd))
+                    {
                         TimeSpan span = DateTime.Parse(NewEventEnd, null, DateTimeStyles.RoundtripKind).ToLocalTime() - DateTimeStart;
                         rec.AppointmentLength = Convert.ToInt32(span.TotalMinutes);
-                        }
+                    }
                     ent.SaveChanges();
                 }
             }
@@ -113,12 +115,12 @@ namespace ClayOnWheels.Models
         {
             try
             {
-                DiaryContainer ent = new DiaryContainer();
+                var ent = new MyDbContext();
                 AppointmentDiary rec = new AppointmentDiary();
                 rec.Title = Title;
                 rec.DateTimeScheduled = DateTime.ParseExact(NewEventDate + " " + NewEventTime, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
                 rec.AppointmentLength = Int32.Parse(NewEventDuration);
-                ent.AppointmentDiary.Add(rec);
+                ent.AppointmentDiaries.Add(rec);
                 ent.SaveChanges();
             }
             catch (Exception)
--- ClayOnWheels/ClayOnWheels/Models/Utils.cs
+++ ClayOnWheels/ClayOnWheels/Models/Utils.cs
@@ -2,43 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ClayOnWheels.Models.EF;
 
 namespace ClayOnWheels.Models
 {
     public class Utils
     {
 
-        public static bool InitialiseDiary() { 
+        public static bool InitialiseDiary() {
             // init connection to database
-            DiaryContainer ent = new DiaryContainer();
+            var ent = new MyDbContext();
             try
-            { 
-                for(int i= 0; i<30; i++){
-                AppointmentDiary item = new AppointmentDiary();
-                // record ID is auto generated
-                item.Title = "Appt: " + i.ToString();
-                item.SomeImportantKey = i;
-                item.StatusENUM = GetRandomValue(0,3); // random is exclusive - we have three status enums
-                if (i <= 5) // create a few appointments for todays date
+            {
+                for (int i = 0; i < 30; i++)
                 {
-                    item.DateTimeScheduled = GetRandomAppointmentTime(false, true);
+                    AppointmentDiary item = new AppointmentDiary();
+                    // record ID is auto generated
+                    item.Title = "Appt: " + i.ToString();
+                    item.SomeImportantKey = i;
+                    item.StatusEnum = GetRandomValue(0, 3); // random is exclusive - we have three status enums
+                    if (i <= 5) // create a few appointments for todays date
+                    {
+                        item.DateTimeScheduled = GetRandomAppointmentTime(false, true);
+                    }
+                    else
+                    {  // rest of appointments on previous and future dates
+                        if (i % 2 == 0)
+                            item.DateTimeScheduled = GetRandomAppointmentTime(true, false); // flip/flop between date ahead of today and behind today
+                        else item.DateTimeScheduled = GetRandomAppointmentTime(false, false);
+                    }
+                    item.AppointmentLength = GetRandomValue(1, 5) * 15; // appoiment length in blocks of fifteen minutes in this demo
+                    ent.AppointmentDiaries.Add(item);
+                    ent.SaveChanges();
                 }
-                else {  // rest of appointments on previous and future dates
-                    if (i % 2 == 0)
-                        item.DateTimeScheduled = GetRandomAppointmentTime(true, false); // flip/flop between date ahead of today and behind today
-                    else item.DateTimeScheduled = GetRandomAppointmentTime(false, false);
-                }
-                item.AppointmentLength = GetRandomValue(1,5) * 15; // appoiment length in blocks of fifteen minutes in this demo
-                ent.AppointmentDiary.Add(item);
-                ent.SaveChanges();
-            }
             }
             catch (Exception)
             {
                 return false;
             }
 
-            return ent.AppointmentDiary.Count() > 0;        
+            return ent.AppointmentDiaries.Count() > 0;        
         }
 
         public static int GetRandomValue(int LowerBound, int UpperBound) {
