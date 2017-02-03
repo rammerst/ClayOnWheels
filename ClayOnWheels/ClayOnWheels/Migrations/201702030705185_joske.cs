3f43e6d33c43c96f2543f55113b40cc36ab156ad
bram barnard <brambarnard@gmail.com>
2017-02-03 08:52:32 +0100

    d

--- ClayOnWheels/ClayOnWheels/Migrations/201702030705185_joske.cs
+++ ClayOnWheels/ClayOnWheels/Migrations/201702030705185_joske.cs
@@ -7,9 +7,9 @@ namespace ClayOnWheels.Migrations
     {
         public override void Up()
         {
-            DropColumn("dbo.AspNetUsers", "Active");
-            AddColumn("dbo.AspNetUsers", "Active", c => c.Boolean(nullable: false));
-            Sql("UPDATE [dbo].[AspNetUsers] SET Active = true");
+            //DropColumn("dbo.AspNetUsers", "Active");
+            //AddColumn("dbo.AspNetUsers", "Active", c => c.Boolean(nullable: false));
+            //Sql("UPDATE [dbo].[AspNetUsers] SET Active = true");
         }
         
         public override void Down()
