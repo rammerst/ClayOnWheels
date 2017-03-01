951aed44da66216330320d2bede68b6f678e7fea
bram barnard <brambarnard@gmail.com>
2017-03-01 21:53:25 +0100

    yess

--- ClayOnWheels/ClayOnWheels/App_Start/Startup.Auth.cs
+++ ClayOnWheels/ClayOnWheels/App_Start/Startup.Auth.cs
@@ -20,21 +20,36 @@ namespace ClayOnWheels
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
+            var cookieProvider = new CookieAuthenticationProvider
+            {
+                // Enables the application to validate the security stamp when the user logs in.
+                // This is a security feature which is used when you change a password or add an external login to your account.  
+                OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+            };
+            // Modify redirect behaviour to convert login URL to relative
+            var applyRedirect = cookieProvider.OnApplyRedirect;
+            cookieProvider.OnApplyRedirect = context =>
+            {
+                var redirectUri = new Uri(context.RedirectUri, UriKind.Absolute);
+
+                if (redirectUri.Scheme == "http" && redirectUri.Host == context.Request.Uri.Host)
+                {
+                    context.RedirectUri = redirectUri.PathAndQuery;
+                }
+
+                applyRedirect(context);
+            };
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             // Configure the sign in cookie
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("https://clayonwheels.apphb.com/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
+                LoginPath = new PathString("Account/Login"),
+                Provider = cookieProvider
+
             });            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
